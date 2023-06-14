130693384894922bf310057161972db631463e61
vikkycodes <okorovictor185@gmail.com>
2023-06-14 23:49:45 +0100

    added new pages on the dashboard

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Demo from "./pages/Demo";
 import About from "./pages/About";
 import Services from "./pages/Services";
+import Web3 from "./pages/Web3";
+import Welcome from "./pages/Welcome";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import DashboardLayout from "./components/DashboardLayout";
-import ConnectWallet from "./pages/ConnectWallet";
 import ViewMedicalRecords from "./pages/ViewMedicalRecords";
 import ViewOtherRecords from "./pages/ViewOtherRecords";
+import ManageAccess from "./pages/ManageAccess";
+import ConnectWallet from "./components/ConnectWalletBtn";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/demo" element={<Demo />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/web3" element={<Web3 />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<DashboardLayout />}>
             <Route
               path="/dashboard/connectwallet"
@@ -27,6 +38,7 @@ const App = () => {
               element={<ViewMedicalRecords />}
             />
             <Route path="/dashboard/medical" element={<ViewOtherRecords />} />
+            <Route path="/dashboard/manageaccess" element={<ManageAccess />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/RecordHistory.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecordHistory = () => {
+  return (
+    <div>
+      <h2>Hello reord history</h2>
+    </div>
+  );
+};
+
+export default RecordHistory;
