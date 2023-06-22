649811abef029fe8a358b42047534af4fe3c29b1
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2023-06-22 14:49:21 +0100

    signup and login code refactoring

--- src/containers/loginpage/Loginpage.jsx
+++ src/containers/loginpage/Loginpage.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 
 import "./loginpage.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Login from "../../assets/login.jpg";
 import Navbar from "../navbar/Navbar";
 import { useSnapshot } from "valtio";
 import state from "../../store/Index";
-import { useSnapshot } from "valtio";
 import { logIN } from "../../api/auth";
 import store from "../../store/Index";
 import { toast } from "react-toastify";
+import Welcome from "../../assets/welcome.jpg";
+import { RiEyeOffFill, RiEyeFill } from "react-icons/ri";
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Loginpage = () => {
   // const [email, setEmail] = useState('');
@@ -34,6 +37,9 @@ const Loginpage = () => {
 
   // };
 
+  const navigate = useNavigate();
+
+  const [loading,setLoading] = useState(false);
   const [loginDetails, setLoginDetails] = useState({
     email: "",
     password: "",
@@ -43,6 +49,15 @@ const Loginpage = () => {
 
   const snap = useSnapshot(state);
 
+
+   // handle loading effect
+   const openLoading = () => {
+    setLoading(true)
+    }
+  
+    const closeLoading = () => {
+      setLoading(false)
+    }
   const handleChange = (ev) => {
     setLoginDetails({
       ...loginDetails,
@@ -115,17 +130,24 @@ const Loginpage = () => {
                   )}
                 </button>
               </div>
-              <button type="submit">Login</button>
-
+              <button type="submit" onClick={openLoading}>Login</button>
+              {
+             loading ? (<Backdrop
+              sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+              open onClick={closeLoading}
+            >
+              <CircularProgress color="inherit" />
+            </Backdrop>) 
+            :
+            null
+            }
               {
                 // (signupDetails.status==="fail") ?
                 // (<span className="successful">{signupDetails.message}</span>) :
                 // (<span className="successful">{signupDetails.status}</span>)
-                loginDetails.status === "success" ? (
-                  <Link to="/dashboard/connectwallet">
-                    <button type="submit">Connect Wallet</button>
-                  </Link>
-                ) : (
+                loginDetails.status === "success" ? 
+                 navigate("/dashboard/connectwallet")
+                : (
                   <span className="successful">{loginDetails.message}</span>
                 )
               }
